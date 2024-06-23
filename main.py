8f77f5dce28ee142672ece3ddcea33db3236b39a
Bruno S.Rosa <brunos.rosa99@gmail.com>
2024-06-23 10:57:26 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Jornada de dados")
+
+
+# Variaveis
+Idade = 23
+nome = "Bruno"
+
+#Print
+
+print(Idade)
+
+# Soma
+
+Nova_idade = Idade + 1
+print(Nova_idade)
+
+#Lista
+lista = [1,2,3,4,5]
+
+#metodos
+Lista_invertida = lista.reverse()
+
